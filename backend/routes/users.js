d8e8107cd77bda38d69425f51a19f1e3864e8c68
Clansou <clement.souplet78@gmail.com>
2023-01-27 14:51:11 +0100

    Fix bug

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -65,3 +65,14 @@ router.post('/', (req,res) => {
         email: req.body.email
     })
 })
+
+router.post('/play/:_id', (req,res) => {
+  User.find({_id:req.params._id}).then(function (users) {
+    const user = new UserPlay()
+    user.name = users.name
+    user.mail = "Clement@free.fr"
+    user.password = "1234"
+    user.save()
+    res.send(user)
+  })
+})
